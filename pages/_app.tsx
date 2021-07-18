ed90b917db0854f2af15947bd829ec6f043fd89a
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-18 23:56:24 +0700

    fix: 🐛 move analytics tracking to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ThemeProvider, StyledEngineProvider } from "@material-ui/core/styles";
 import createCache from "@emotion/cache";
 
+import * as ga from "lib/analytics/google-analytics";
 import { lightTheme } from "lib/theme";
 
 const cache = createCache({ key: "css" });
 cache.compat = true;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <StyledEngineProvider injectFirst>
       <Head>
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,24 +1,6 @@
-import { PropsWithChildren, useEffect } from "react";
-import { useRouter } from "next/router";
-import * as ga from "lib/analytics/google-analytics";
+import { PropsWithChildren } from "react";
 
 const AppLayout = ({ children }: PropsWithChildren<{}>) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return <>{children}</>;
 };
 
